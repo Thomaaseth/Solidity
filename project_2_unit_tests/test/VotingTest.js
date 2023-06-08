f9e9bb89977672581c1463015203d1423e08db41
thomaaseth <thomas.demathieu@gmail.com>
2023-06-08 22:21:30 +0200

    finishing voting tests

--- project_2_unit_tests/test/VotingTest.js
+++ project_2_unit_tests/test/VotingTest.js
@@ -18,7 +18,7 @@ describe('Test Voting', function() {
             assert.equal(owner.address, theOwner);
         });
 
-    // Add voter tests //
+    // Add voter tests // 
 
     describe('Adding voters', function() {
         it('owner should be able to add voters in RegisteringVoters state', async function(){
@@ -43,11 +43,12 @@ describe('Test Voting', function() {
         })  
     });
 
-    // Proposal tests //
+    // Proposal tests // (test genesis proposal??)
 
     describe('Registering proposal', function() {
         let voter1;
-        describe('before/after starting proposal registration', function(){
+        
+        describe('before starting/after closing proposal registration', function(){
         
             beforeEach(async function() {
                 let signers = await ethers.getSigners();
@@ -64,7 +65,7 @@ describe('Test Voting', function() {
                 await expect(voting.connect(voter1).addProposal('Proposal 2')).to.be.revertedWith('Proposals are not allowed yet');
             })
                 
-        describe('after starting proposal registration', function(){
+        describe('after starting proposal registration', function() {
             
             beforeEach(async function() {
                 let signers = await ethers.getSigners();
@@ -89,7 +90,125 @@ describe('Test Voting', function() {
 
     // Voting tests //
 
-    // describe('')
+    describe('Vote registering', function() {
+        let voter1, voter2, voter3;
+        
+        describe('before starting/after closing voting session', function() {
+        
+        beforeEach(async function() {
+            let signers = await ethers.getSigners();
+            owner = signers[0];
+            voter1 = signers[1];
+            voter2 = signers[2];
+            voter3 = signers[3];
+            voter4 = signers[4];
+            voter5 = signers[5];
+            voter6 = signers[6];
+        
+            await voting.connect(owner).addVoter(voter1.address);
+            await voting.connect(owner).addVoter(voter2.address);
+            await voting.connect(owner).addVoter(voter4.address);
+            await voting.connect(owner).addVoter(voter5.address);
+            await voting.connect(owner).addVoter(voter6.address);
+        });
+        it('check voters are registered', async function() {
+            await expect(voting.connect(owner).addVoter(voter1.address)).to.be.revertedWith('Already registered');
+            await expect(voting.connect(owner).addVoter(voter2.address)).to.be.revertedWith('Already registered');
+        });
+        it('tries to vote but session not started', async function() {
+            let [_, __, voter2] = await ethers.getSigners();
+            await expect(voting.connect(voter1).setVote(1)).to.be.revertedWith('Voting session havent started yet');
+            await expect(voting.connect(voter2).setVote(1)).to.be.revertedWith('Voting session havent started yet');
+        });
+        it('tries to vote but session ended', async function() {
+            let [_, __, voter2] = await ethers.getSigners();
+            await voting.connect(owner).startProposalsRegistering();
+            await voting.connect(voter1).addProposal('Proposal 1');
+            await voting.connect(owner).endProposalsRegistering();
+            await voting.connect(owner).startVotingSession();
+            await voting.connect(owner).endVotingSession();
+            await expect(voting.connect(voter1).setVote(1)).to.be.revertedWith('Voting session havent started yet');
+            await expect(voting.connect(voter2).setVote(1)).to.be.revertedWith('Voting session havent started yet');
+        });
 
-})
-})
+        describe('after starting voting session', function() {
+
+            beforeEach(async function() {
+                let signers = await ethers.getSigners();
+                voter1 = signers[1];
+                voter2 = signers[2];
+                voter3 = signers[3];
+                voter4 = signers[4];
+                voter5 = signers[5];
+                voter6 = signers[6];
+                await voting.connect(owner).startProposalsRegistering();
+                await voting.connect(voter1).addProposal('Proposal 1');
+                await voting.connect(voter2).addProposal('Proposal 2');
+                await voting.connect(owner).endProposalsRegistering();
+                await voting.connect(owner).startVotingSession();
+            })
+            it('register vote', async function() {
+                await expect(voting.connect(voter1).setVote(0))
+                .to.emit(voting, 'Voted')
+                .withArgs(voter1.address, 0);
+            })
+            it('checks cant vote on inexistant proposal', async function() {
+                await expect(voting.connect(voter1).setVote(4)).to.be.revertedWith('Proposal not found');
+            });
+            it('checks cant vote twice', async function() {
+                await expect(voting.connect(voter1).setVote(0))
+                .to.emit(voting, 'Voted')
+                .withArgs(voter1.address, 0);
+                await expect(voting.connect(voter1).setVote(0)).to.be.revertedWith('You have already voted');
+            });
+            it('tries to vote but not registered as voter', async function() {
+                await expect (voting.connect(voter3).setVote(0)).to.be.revertedWith("You're not a voter");
+            });
+            // We assume Owner isn't allowed to vote by default
+            it('owner tries to vote but not registered as a voter', async function() {
+                await expect (voting.connect(owner).setVote(0)).to.be.revertedWith("You're not a voter");
+            });
+            it('total vote counts increment properly', async function() {
+                let filter = voting.filters.Voted();
+                let initLogs = await voting.queryFilter(filter);
+                let initVoteCount = initLogs.length;
+                await voting.connect(voter1).setVote(0);
+                await voting.connect(voter2).setVote(0);
+                await voting.connect(voter4).setVote(0);
+                await voting.connect(voter5).setVote(0);
+                await voting.connect(voter6).setVote(1);
+                let finalLogs = await voting.queryFilter(filter);
+                let finalVoteCount = finalLogs.length;
+                expect(finalVoteCount).to.equal(initVoteCount + 5);
+            });
+        });
+
+        // Check that Owner can register as voter and cast a vote
+        describe('check owner can vote only if he registers as voter', function() {
+        
+        beforeEach(async function() {
+            let signers = await ethers.getSigners();
+            owner = signers[0];
+            await voting.connect(owner).addVoter(owner.address);
+            await voting.connect(owner).startProposalsRegistering();
+            await voting.connect(owner).endProposalsRegistering();
+            await voting.connect(owner).startVotingSession();
+        })
+            it('owner can vote if he registered himself as voter', async function() {
+                await expect(voting.connect(owner).setVote(0))
+                .to.emit(voting, 'Voted')
+                .withArgs(owner.address, 0);
+             })
+
+        })
+    });
+
+    // Test tally votes
+
+
+    
+            
+    });
+    });
+});
+
